12555eda59ffbf694594d1424d9c7c3947923f1d
Danyang Li <dyang.l@outlook.com>
2023-10-25 03:40:09 +0100

    update portfolio1025

--- test.js
+++ test.js
@@ -1,9 +1,34 @@
-window.onload = function() {
-    setTimeout(function() {
-        document.querySelector('.lightbeam').style.animationPlayState = "running";
-    }, 2000);
+document.addEventListener('DOMContentLoaded',function(){
+    const helloText = document.querySelector(".text_container")
 
-    setTimeout(function() {
-        document.querySelector('.screen').style.opacity = "1";
-    }, 5000);
-}
+    const careers = ["Software Developer!","VR Documentary Filmmaker!"];
+
+    let careerIndex = 0;
+    let characterIndex = 0;
+    updateText();
+
+    function updateText(){
+        
+        characterIndex++
+        helloText.innerHTML =`<h1>Hello! I am Danyang, a <br><br>${careers[careerIndex].slice(0,characterIndex)}</h1>`;
+
+        if(characterIndex === careers[careerIndex].length){
+            careerIndex++;
+            characterIndex = 0;
+            if(careerIndex === careers.length){
+                careerIndex = 0;
+            }
+            setTimeout(updateText,1000);
+        } else {
+            setTimeout(updateText,150);
+        }
+            
+  
+    }
+
+
+
+})
+
+
+
