d6a333b617162f1347f214dcba0590539ff4e23f
Danyang Li <dyang.l@outlook.com>
2023-11-09 13:09:43 +0000

    some improve

--- Portfolio.js
+++ Portfolio.js
@@ -1,8 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
     const helloText = document.querySelector(".text_container")
     const careers = ["Software Developer!", "VR Documentary Filmmaker!"];
-    const scriptURL = 'https://script.google.com/macros/s/AKfycbxTBMDolg32SoE5QpdrvgoNpWR0CCevQf9xhU5jBJLJ0ngmQ7amqX1ngIu9KvLPiDrv/exec'
-    const form = document.forms['submit-to-google-sheet']
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbysdslR5lI7DjeRsOs4xD7PrArWk6JtbY8gXHij-MBKr6cImPAErzMx1HGIMurmF8Ko/exec'
+    const form = document.forms['contact-form']
     const msgSpan = document.querySelector('.msg');
 
     let careerIndex = 0;
